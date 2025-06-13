65bfea5ccd03e01bd6109ac1ded83e159ba9806b
Obaedur Rahman <arnobahemd12345@gmail.com>
2025-06-12 23:59:06 -0400

    Will start writing child process code

--- unix/childProcess.js
+++ unix/childProcess.js
@@ -1,1 +1,8 @@
 // Will write child process code here
+const { spawn, exec } = require("node:child_process");
+
+const subprocess = spawn("echo", ["some string"]);
+
+subprocess.stdout.on("data", (data) => {
+  console.log(data.toString("utf-8"));
+});
